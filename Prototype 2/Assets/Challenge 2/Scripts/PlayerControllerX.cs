bd2dedcb533f1a37b309271ac2af7620a4ef2ec3
alexCarwin <alexeilatushkin@yandex.ru>
2022-07-07 10:58:31 +0300

    Delete meta files

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float lastTime;
+    private float delayBetweenAction = 1.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > lastTime + delayBetweenAction)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                lastTime = Time.time;
+            }
+        }
+        // On spacebar press, send dog
+
+    }
+}
